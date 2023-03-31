ac09f34176695e09e120213f461ba41ca6b2da7c
CypressJack <wadecroft@gmail.com>
2023-04-01 00:45:48 +0100

    update plugin info

--- next-wp.php
+++ next-wp.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:       Next WP
- * Plugin URI:        https://github.com/Stikky-Media/next-wp-plugins
+ * Plugin URI:        https://github.com/cloak-labs/next-wp-plugin
  * Description:       Base plugin for Next-WP configuration 
  * Author:            Cloak Labs
- * Author URI:        https://www.stikkymedia.com/
+ * Author URI:        https://github.com/cloak-labs
  * Version:           0.5.0
  * Requires at least: 5.5
  * Requires PHP:      7.0
@@ -17,5 +17,6 @@ require_once __DIR__ . '/admin/settings.php';
 require_once __DIR__ . '/src/functions.php';
 require_once __DIR__ . '/rest-api/rest-api.php';
 require_once __DIR__ . '/seo/seo.php';
+
 
 
