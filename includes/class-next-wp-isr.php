159b2c30de8332c656f725b96c69653cb758b793
Wade Croft <wadecroft@gmail.com>
2023-04-05 16:15:46 -0700

    add ISR class

---
+++ includes/class-next-wp-isr.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/cloak-labs
+ * @since      1.0.0
+ *
+ * @package    Next_Wp
+ * @subpackage Next_Wp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run ISR.
+ *
+ * @since      1.0.0
+ * @package    Next_Wp
+ * @subpackage Next_Wp/includes
+ * @author     Cloak Labs <wade@stikkymedia.com>
+ */
+class Next_Wp_Isr
+{
+
+  public function __construct()
+  {
+    $this->enable_isr();
+  }
+
+  /**
+   * On-demand Incremental Static Regeneration (ISR) 
+   * 
+   * rebuild static pages/posts immediately upon saving your changes in WP
+   * 
+   * 
+   * @param $post_ID
+   * @param $post
+   * 
+   * @return void
+   * 
+   * @since    1.0.0
+   */
+  public static function revalidate_on_save($post_ID, $post)
+  {
+    $front_end_url = get_frontend_url();
+    $revalidate_api_route = NEXT_WP_REVALIDATE_API_ROUTE ? NEXT_WP_REVALIDATE_API_ROUTE : 'revalidate';
+    // manually add environment URLs to this array if you wish to enable on-demand ISR for that environment (useful for testing one-off Vercel deployments or when running a production build locally) 
+    $environments_to_revalidate = [$front_end_url, "http://localhost:3000"];
+    $slug = $post->post_name;
+    $type = $post->post_type;
+    $secret = get_preview_secret();
+    foreach ($environments_to_revalidate as $url) {
+      try {
+        wp_remote_get("{$url}/api/{$revalidate_api_route}/{$slug}?post_type={$type}&secret={$secret}");
+      } catch (Exception $e) {
+        echo 'Error while regenerating static page for url "', $url, '" -- error message: ', $e->getMessage(), "\n";
+      }
+    }
+  }
+
+
+  /**
+   * On-demand Incremental Static Regeneration (ISR) 
+   * 
+   * rebuild static pages/posts immediately upon saving your changes in WP
+   * 
+   * 
+   * @param bool NEXT_WP_ENABLE_ISR
+   * @param bool NEXT_WP_REVALIDATE_API_ROUTE
+   * 
+   * @return void
+   * 
+   * @since    1.0.0
+   */
+  public function enable_isr()
+  {
+    if (defined(NEXT_WP_ENABLE_ISR)) {
+      if (NEXT_WP_ENABLE_ISR === TRUE) {
+        add_action('save_post_page', array($this, 'revalidate_on_save'), 10, 2);
+      }
+    }
+  }
+}
