7fd0bf6faa43e9a93c69fef87a344f79f838866c
Wade Croft <wadecroft@gmail.com>
2023-04-05 15:42:05 -0700

    add oop boilerplate

---
+++ admin/partials/next-wp-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://https://github.com/cloak-labs
+ * @since      1.0.0
+ *
+ * @package    Next_Wp
+ * @subpackage Next_Wp/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-next-wp-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/cloak-labs
+ * @since      1.0.0
+ *
+ * @package    Next_Wp
+ * @subpackage Next_Wp/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Next_Wp
+ * @subpackage Next_Wp/includes
+ * @author     Cloak Labs <wade@stikkymedia.com>
+ */
+class Next_Wp_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
